cc4fbd9156cc5b8ba6f2e38c31e5f0335a57717f
Saransh Jindal <saranshj35@gmail.com>
2024-03-25 04:48:03 +0530

    home screen updated

--- lib/home.dart
+++ lib/home.dart
@@ -6,14 +6,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[300],
+      backgroundColor: const Color.fromARGB(255, 96, 188, 180),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/images/quiz-logo.png'),
-          const Text('Learn Flutter the fun way!'),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            color: const Color.fromARGB(255, 91, 93, 95),
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 91, 93, 95),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
           OutlinedButton(
             onPressed: () {},
-            child: const Text('Start Quiz'),
+            child: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 91, 93, 95),
+              ),
+            ),
           ),
         ],
       ),
